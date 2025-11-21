0a0331654eba61a912286261143212712a307d1f
Mauro De Francesco <mdefrancesco@studenti.apuliadigitalmaker.it>
2025-11-21 13:21:44 +0100

    Aggiunta progetto Spring

---
+++ src/main/java/it/its/eventu/EventuApplication.java
@@ -0,0 +1,13 @@
+package it.its.eventu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventuApplication.class, args);
+	}
+
+}
